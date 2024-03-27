e5b0d6b4afac6cfb625a12aa345bbaa73f6684b0
SixuLi97 <sli739@wisc.edu>
2024-03-26 22:48:50 -0500

    Run KDE generation.

--- bin/run_simulations.sh
+++ bin/run_simulations.sh
@@ -37,7 +37,7 @@ for seed in {0..8}
 do
   python "${SRC}"/main.py \
       --experiment_name "KDE_generation_cifar10" \
-      --scaling_factor 0.1 \
+      --scaling_factor 2.0 \
       --seed "$seed"
 done
 
